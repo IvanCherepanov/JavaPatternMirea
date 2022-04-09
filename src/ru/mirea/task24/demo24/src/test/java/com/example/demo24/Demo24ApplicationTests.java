33ed777054d6ffc34d71228ce61655a7e675418f
Ivan <iv.cherepanov@yandex.ru>
2022-04-09 14:58:11 +0300

    24 task is done

--- src/ru/mirea/task24/demo24/src/test/java/com/example/demo24/Demo24ApplicationTests.java
+++ src/ru/mirea/task24/demo24/src/test/java/com/example/demo24/Demo24ApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.demo24;
 
+
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class Demo24ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+class Demo24ApplicationTests {
 
 }
